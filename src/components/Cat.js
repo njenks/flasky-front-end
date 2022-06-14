a08d1e1123da49eb5ec65cc5c67944c49b071b26
Cherise <cherise.holmes@adadevelopersacademy.org>
2022-06-14 11:37:20 -0700

    otters livecode 6/14

---
+++ src/components/Cat.js
@@ -0,0 +1,14 @@
+import "./Cat.css";
+
+const Cat = () => {
+  const catName = "Jeff THEE Cat";
+  const catSaying = "Rainbows 5evah";
+  return (
+    <div className="rainbow-box">
+      <h3> {catName} </h3>
+      <p> {catSaying} </p>
+    </div>
+  );
+};
+
+export default Cat;
