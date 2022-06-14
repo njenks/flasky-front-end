9227f80a00e1aa79b377ffc30dfa6963bb1cd2ec
Auberon López <auberon@adadev.org>
2022-06-14 14:59:48 -0700

    Yay state

--- src/components/Driver.js
+++ src/components/Driver.js
@@ -1,15 +1,27 @@
 import "./Driver.css";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const Driver = (props) => {
+  const [handsome, setHandsome] = useState(props.handsome);
+
   return (
     <div>
       <h2 className="driver__name">{props.name}</h2>
       <ul>
         <li>Team: {props.team}</li>
         <li>Country: {props.country}</li>
-        <li>Handsome: {props.handsome.toString()}</li>
+        <li>Handsome: {handsome.toString()}</li>
       </ul>
+      <button
+        onClick={(e) => {
+          console.log(e);
+          setHandsome(!handsome);
+        }}
+      >
+        Change attractiveness
+      </button>
+      <button>Delete</button>
     </div>
   );
 };
@@ -19,7 +31,6 @@ Driver.propTypes = {
   team: PropTypes.string.isRequired,
   country: PropTypes.string.isRequired,
   handsome: PropTypes.bool.isRequired,
-  something: PropTypes.string.isRequired,
 };
 
 export default Driver;
