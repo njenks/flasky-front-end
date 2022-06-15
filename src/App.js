ce008f8a41c91fad9f2ba82fd4635976be500a5b
Audrey Andoy <audreylandoy@gmail.com>
2022-06-15 10:49:36 -0700

    props roundtable work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import './App.css';
 import { CatList } from './components/CatList';
 
+const catData = [
+  {
+    name: "Whiskers",
+    chipNumber: "13579",
+  },
+  {
+    name: "Fang",
+    chipNumber: "82672",
+  },
+  {
+    name: "Beetle",
+    chipNumber: "02468",
+  },
+  {
+    name: "Loki",
+    chipNumber: "3454353",
+  }
+];
+
+const dog_data = [
+  {
+    name: "Wishbone",
+    favoriteToy: "Squeaker",
+    chipNumber: "82373",
+  },
+  {
+    name: "Appa",
+    favoriteToy: "Ball",
+    chipNumber: "29238",
+  },
+  {
+    name: "Jimminy",
+    favoriteToy: "Bone",
+    chipNumber: "72902",
+  }
+];
+
+
 function App() {
   return (
     <main>
       <h1>The Cat Corral</h1>
-      <CatList></CatList>
+      <CatList cats={catData}></CatList>
     </main>
   );
 }
