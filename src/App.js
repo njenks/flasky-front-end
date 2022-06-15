32d12ef28d2d144d5a800998de868245b8173374
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2022-06-15 10:38:12 -0700

    Use props to pass data to components from App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import './App.css';
-import Cat from './components/Cat';
-import CatList from './components/CatList';
+import "./App.css";
+import Cat from "./components/Cat";
+import CatList from "./components/CatList";
 
 function App() {
+  const catData = [
+    {
+      name: "Whiskers",
+      caretaker: "Farah",
+      petCount: 1357,
+    },
+    {
+      name: "Fang",
+      caretaker: "Luciana",
+      petCount: 8267,
+    },
+    {
+      name: "Beetle",
+      caretaker: "Aya",
+      petCount: 2468,
+    },
+  ];
+
   return (
-  <main>
-    <h1>List of Cats</h1>
-    <Cat />
-  </main>
+    <main>
+      <h1>List of Cats</h1>
+      <CatList catData={catData} />
+    </main>
   );
 }
 
--- src/components/CatList.js
+++ src/components/CatList.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import Cat from './Cat';
+import React from "react";
+import Cat from "./Cat";
+import PropTypes from "prop-types";
 
-const CatList = () => {
+const CatList = (props) => {
+  const catComponents = props.catData.map((cat) => {
     return (
-        <ul>
-            <Cat />
-            <Cat />
-            <Cat />
-        </ul>
+      <Cat name={cat.name} caretaker={cat.caretaker} petCount={cat.petCount} />
     );
+  });
+
+  return (
+    <section>
+      <h2>Cat count: {props.catData.length}</h2>
+      <ul>{catComponents}</ul>;
+    </section>
+  );
+};
+
+CatList.propTypes = {
+  catData: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default CatList;
