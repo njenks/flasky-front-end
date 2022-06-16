028e1cc37f36cdcafc74e99a33f4a6c566999f8a
Trenisha Goslee <60710670+tgoslee@users.noreply.github.com>
2022-06-16 09:18:23 -0400

    petting a cat

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
-import CatList from './components/CatList';
+import "./App.css";
+import CatList from "./components/CatList";
 
 function App() {
+  const catData = [
+    {
+      name: "Whiskers",
+      caretaker: "Farah",
+    },
+    {
+      name: "Fang",
+      caretaker: "Luciana",
+    },
+    {
+      name: "Beetle",
+      caretaker: "Aya",
+    },
+  ];
+
   return (
-  <main>
-    <h1>List of Cats</h1>
-    <CatList />
-  </main>
+    <main>
+      <h1>List of Cats</h1>
+      <CatList catData={catData} />
+    </main>
   );
 }
 
--- src/components/CatList.js
+++ src/components/CatList.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import Cat from './Cat';
+import React from "react";
+import Cat from "./Cat";
+import PropTypes from "prop-types";
 
-const CatList = () => {
+const CatList = (props) => {
+
+  const catComponents = props.catData.map((cat) => {
     return (
-        <ul>
-            <Cat />
-            <Cat />
-            <Cat />
-        </ul>
+      <Cat name={cat.name} caretaker={cat.caretaker} />
     );
+  });
+   
+  return (
+    <section>
+      <h2>Cat count: {props.catData.length}</h2>
+      <ul>{catComponents}</ul>;
+    </section>
+  );
+};
+
+CatList.propTypes = {
+  catData: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default CatList;
