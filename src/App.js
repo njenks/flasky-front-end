00209e460dc71a7423dac345a446e4cfe7691494
Trenisha Goslee <60710670+tgoslee@users.noreply.github.com>
2022-06-16 09:04:13 -0400

    stash

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Cat from './components/Cat';
 import CatList from './components/CatList';
 
 function App() {
   return (
   <main>
     <h1>List of Cats</h1>
-    <Cat />
+    <CatList />
   </main>
   );
 }
