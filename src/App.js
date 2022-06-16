44f94c7da0dc3247a377ccb6b0ec35ad40b9f95d
Jasmine Lopez <jasmine@adadevelopersacademy.org>
2022-06-16 11:35:28 -0700

    Adding State!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import CatList from "./components/CatList";
 import Cat from "./components/Cat";
+import {useState} from "react";
 
 function App() {
+    const [placeholder, setPlaceholder] = useState("Hello");
     const catData1 = [
         {id:1, name:"Jeff THEE Cat", saying:"rainbows 5evah", age:5, color:"tabby"},
         {"id":2, "name":"Lily", "saying":"cry", "age":5, "color":"tabby"},
@@ -14,11 +16,16 @@ function App() {
         {"id":4, "name":"Prince", "saying":"meow", "age":5, "color":"tuxedo"},
     ]
 
+    const handleAppClick = () => {
+        setPlaceholder(placeholder + "!");
+    }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1> Otters Flasky </h1>
       </header>
+      <button onClick={handleAppClick}>{placeholder}</button>
       <main>
         <CatList catData={catData1}/>
         <CatList catData={catData2} />
--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,17 +1,31 @@
 import "./Cat.css";
 import PropTypes from "prop-types";
-
+import {useState} from "react";
 
 const Cat = (props) => {
-  const catName = props.name;
-  const catSaying = props.saying;
-  const catAge = props.age;
-  const catColor = props.color;
-  return (
+
+    const [catAge, setCatAge] = useState(props.age);
+    const [catSaying, setCatSaying] = useState(props.saying);
+
+    const catName = props.name;
+    const catColor = props.color;
+    const makeCatOlder = () => {
+        setCatAge(catAge + 1);
+    };
+
+    const changeCatSaying = (event) => {
+        let newSaying = event.target.value;
+        if(newSaying === "") {
+            newSaying = "Hello!"
+        }
+        setCatSaying(newSaying)
+    }
+    return (
     <div className="rainbow-box">
       <h3> {catName} </h3>
       <p> {catSaying} </p>
-      <p> {catAge}</p>
+      <p> Change Saying: <input type="text" onChange={changeCatSaying} /></p>
+      <p> {catAge} <button onClick={makeCatOlder}>🙀</button></p>
       <p> {catColor}</p>
     </div>
   );
